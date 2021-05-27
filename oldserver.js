efab56cf11735123791cc2be136e9c07bfafd471
19khowes <72945859+19khowes@users.noreply.github.com>
2021-05-27 12:37:50 -0600

    foldering functionality
    Changed previous functional server.js to oldserver.js and creater new server.js which handles new buttons for getting folder from a canvas course and then you make a request for files from a specific folder by id rather than all files from a specific course

--- oldserver.js
+++ oldserver.js
@@ -14,25 +14,11 @@ app.use(express.static('public'));
 
 app.use(express.json());
 
-app.post('/folders', async (request, response) => {
-    const course_id = (request.body.course)
+app.post('/api', async (request, response) => {
+    const course_id = (request.body.value)
     console.log(course_id);
 
-    let fetch_url = `https://usu.beta.instructure.com/api/v1/courses/${course_id}/folders/?access_token=${token}&per_page=${per_page}`;
-    const fetch_response = await fetch(fetch_url);
-    //const fetch_response = await fetch(`https://usu.beta.instructure.com/api/v1/courses/${course_id}/files?access_token=1009~xR2kix0bixBr0Wb5zfibZTr3MBezYel1urw8dOibwVD9FPqQiQ0FP0jHFm5BOEjl&per_page=1000`)
-    const json = await fetch_response.json();
-    console.log(fetch_url);
-
-    response.json(json);
-});
-
-app.post('/api', async (request, response) => {
-    const course_id = (request.body.course)
-    const folder = (request.body.folder)
-    console.log(folder);
-
-    let fetch_url = `https://usu.beta.instructure.com/api/v1/folders/${folder}/files?access_token=${token}&per_page=${per_page}`;
+    let fetch_url = `https://usu.beta.instructure.com/api/v1/courses/${course_id}/files?access_token=${token}&per_page=${per_page}`;
     const fetch_response = await fetch(fetch_url);
     //const fetch_response = await fetch(`https://usu.beta.instructure.com/api/v1/courses/${course_id}/files?access_token=1009~xR2kix0bixBr0Wb5zfibZTr3MBezYel1urw8dOibwVD9FPqQiQ0FP0jHFm5BOEjl&per_page=1000`)
     const json = await fetch_response.json();
--- public/client.js
+++ public/client.js
@@ -1,11 +1,19 @@
 const submit = document.getElementById('btn');
 const clear_btn = document.getElementById('clear-btn');
+const folders_btn = document.getElementById('folders-btn');
+const clear_folders_btn = document.getElementById('clear-folders-btn');
+const folder_input = document.getElementById('folder-input');
 const course_input = document.getElementById('course-input');
 const images_container = document.getElementById('images-container');
+const folders_container = document.getElementById('folders-container');
 
 submit.addEventListener('click', async () => {
-    const value = course_input.value;
-    const data = { value }
+    const course = course_input.value;
+    const folder = folder_input.value;
+    const data = {
+        course,
+        folder
+    };
 
 
     // options for fetch to /api
@@ -22,11 +30,19 @@ submit.addEventListener('click', async () => {
     const response_json = await post_response.json();
     response_json.forEach(item => {
         const root = document.createElement('div');
-        const new_image = document.createElement('img');
+        let new_image;
 
-        new_image.src = item.url;
-        new_image.width = 500;
-        new_image.classList.add('images');
+        if ((item.mime_class) == "pdf") {
+            new_image = document.createElement('p');
+
+            new_image.innerHTML = `${item.display_name}`;
+        } else {
+            new_image = document.createElement('img');
+
+            new_image.src = item.url;
+            new_image.width = 500;
+            new_image.classList.add('images');
+        }
 
         root.append(new_image);
         images_container.append(root);
@@ -35,7 +51,42 @@ submit.addEventListener('click', async () => {
     })
 });
 
+folders_btn.addEventListener('click', async () => {
+    const course = course_input.value;
+    const data = {
+        course
+    }
+
+
+    // options for fetch to /api
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    }
+
+    const post_response = await fetch('/folders', options);
+    const response_json = await post_response.json();
+    response_json.forEach(item => {
+        const root = document.createElement('div');
+        const new_folder = document.createElement('p');
+
+        new_folder.innerHTML = `<strong>id:</strong> ${item.id} <strong>path:</strong> ${item.full_name}`;
+
+        root.append(new_folder);
+        folders_container.append(root);
+
+        console.log(item.url)
+    })
+});
+
 clear_btn.addEventListener('click', () => {
     images_container.innerHTML = "";
-})
+});
 
+clear_folders_btn.addEventListener('click', () => {
+    folders_container.innerHTML = "";
+});
