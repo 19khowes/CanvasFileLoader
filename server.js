43f409bd9b02748713db988524fb541f3228a0e8
19khowes <72945859+19khowes@users.noreply.github.com>
2021-05-28 10:30:40 -0600

    Changed from beta
    Changed to use usu.instructure, rather than usu.beta.instructure and regenerated api token

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 const url = "https://usu.beta.instructure.com/api/v1/courses/615872/files";
-const token = "1009~vRn8YALIrhRGoaJ5SD9lytb3SbneKflS5WRivVbmsC9ZVHHCsrxzJPQjhCbgPk8i";
+const token = "1009~vTRAIIcaIXQSXNGr0ld97MY0chcOCBLJw5l9woLT9Vm2m8ZVghSniBMSsdLEfg1J";
 const per_page = 1000;
 const port = 3000;
 
@@ -18,7 +18,7 @@ app.post('/folders', async (request, response) => {
     const course_id = (request.body.course)
     console.log(course_id);
 
-    let fetch_url = `https://usu.beta.instructure.com/api/v1/courses/${course_id}/folders/?access_token=${token}&per_page=${per_page}`;
+    let fetch_url = `https://usu.instructure.com/api/v1/courses/${course_id}/folders/?access_token=${token}&per_page=${per_page}`;
     const fetch_response = await fetch(fetch_url);
     //const fetch_response = await fetch(`https://usu.beta.instructure.com/api/v1/courses/${course_id}/files?access_token=1009~xR2kix0bixBr0Wb5zfibZTr3MBezYel1urw8dOibwVD9FPqQiQ0FP0jHFm5BOEjl&per_page=1000`)
     const json = await fetch_response.json();
@@ -32,7 +32,7 @@ app.post('/api', async (request, response) => {
     const folder = (request.body.folder);
     console.log(folder);
 
-    let fetch_url = `https://usu.beta.instructure.com/api/v1/folders/${folder}/files?access_token=${token}&per_page=${per_page}`;
+    let fetch_url = `https://usu.instructure.com/api/v1/folders/${folder}/files?access_token=${token}&per_page=${per_page}`;
     const fetch_response = await fetch(fetch_url);
     //const fetch_response = await fetch(`https://usu.beta.instructure.com/api/v1/courses/${course_id}/files?access_token=1009~xR2kix0bixBr0Wb5zfibZTr3MBezYel1urw8dOibwVD9FPqQiQ0FP0jHFm5BOEjl&per_page=1000`)
     const json = await fetch_response.json();
